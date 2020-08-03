aa25d53b890e4131d264859bfdb4f5600000cda6
Alireza <algor.devs@gmail.com>
2020-08-03 18:38:06 +0430

    Added prettifier for code highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
 var swiper = new Swiper('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
     },
-  });
+  },
+});
+
+
+
+const preElements = document.getElementsByTagName('pre');
+const codeElements = document.getElementsByTagName('code');
+for (let preElemet of preElements) {
+  preElemet.classList.add("prettyprint");
+}
+for (let codeElement of codeElements) {
+  codeElement.classList.add("prettyprint");
+}
+
+
+
+
