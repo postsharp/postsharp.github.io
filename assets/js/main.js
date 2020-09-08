49a14482394b378a791d55053e2db6586b2465ed
Alireza <algor.devs@gmail.com>
2020-09-08 23:48:10 +0430

    Added arrows to the latest posts slider in homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
