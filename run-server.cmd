86adc1660614a0cb018cea5aaf724450da2c01f3
Gael Fraiteur <gael@postsharp.net>
2020-11-19 14:37:08 +0100

    Fix title

---
+++ run-server.cmd
@@ -0,0 +1,5 @@
+REM To run from Windows:
+REM  1. Install Ruby for Windows with RubyInstaller: https://jekyllrb.com/docs/installation/windows/
+REM  2. In the Ruby prompt, do: gem install jekyll bundler
+REM  3. Then run this script in a Ruby prompt
+bundle exec jekyll server --unpublished 
